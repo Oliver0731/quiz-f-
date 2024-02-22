4a8063b03d5eb111e9fb5a20f6bd90eab30abaa3
Oliver0731 <o.townell@share.epsb.ca>
2024-02-22 09:00:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-document.getElementById("btn").addEventListener("click", calc);
-
-
-
-
 function markQuestion(inputID, outputID, correctAnswer) {
 if (inputID === correctAnswer){
     document.getElementById(outputID).innerHTML = "correct";
@@ -12,16 +7,19 @@ if (inputID === correctAnswer){
     return 0;
 }
 }
-
 function calc(){
+    let luckNum = document.getElementById("luckNum").value;
+    let lowNum = document.getElementById("lowNum").value;
+    let highNum = document.getElementById("highNum").value;
+    let FiveNum = document.getElementById("5Num").value;
 let score = 0;
-score += markQuestion("luckNum", "output1", "7");
-score += markQuestion("lowNum", "output2", "1");
-score += markQuestion("highNum", "output3", "10");
-score += markQuestion("5Num", "output4", "5");
-
+score += markQuestion(luckNum, "output1", "7");
+score += markQuestion(lowNum, "output2", "1");
+score += markQuestion(highNum, "output3", "10");
+score += markQuestion(FiveNum, "output4", "5");
 document.getElementById("percent").innerHTML = score / 4;
 }
 
 
 
+document.getElementById("btn").addEventListener("click", calc);
